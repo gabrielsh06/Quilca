4a063fcc83d07241d113dd8c83d23f72a0a99a8e
gabrielsh06 <shgabriel06@gmail.com>
2026-04-25 00:57:59 -0500

    feat: add text style model

--- src/main/java/com/quilca/model/TextStyle.java
+++ src/main/java/com/quilca/model/TextStyle.java
@@ -9,4 +9,12 @@ public record TextStyle(
     public static TextStyle forDefault() {
         return new TextStyle(false, false, 14, "black");
     }
+
+    public TextStyle withBold(boolean bold) {return new TextStyle(bold, this.italic, this.size, this.color);}
+
+    public TextStyle withItalic(boolean italic) {return new TextStyle(this.bold, italic, this.size, this.color);}
+
+    public TextStyle withSize(int size) {return new TextStyle(this.bold, this.italic, size, this.color);}
+
+    public TextStyle withColor(String color) {return new TextStyle(this.bold, this.italic, this.size, color);}
 }
