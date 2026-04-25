fd3974538cade47d4829b29e8b81b15fb651212f
gabrielsh06 <shgabriel06@gmail.com>
2026-04-25 01:09:22 -0500

    feat: add TopBarController and migrate I/O methods

--- src/main/java/com/quilca/controller/EditorController.java
+++ src/main/java/com/quilca/controller/EditorController.java
@@ -4,12 +4,9 @@ import com.quilca.model.TextStyle;
 import com.quilca.service.EditorService;
 import javafx.fxml.FXML;
 import javafx.scene.layout.StackPane;
-import javafx.stage.FileChooser;
 import org.fxmisc.flowless.VirtualizedScrollPane;
 import org.fxmisc.richtext.StyledTextArea;
 import org.springframework.stereotype.Component;
-
-import java.io.File;
 
 @Component
 public class EditorController {
@@ -29,27 +26,5 @@ public class EditorController {
                 editorService.getEditorArea()
         );
         editorContainer.getChildren().add(scroll);
-    }
-
-    @FXML
-    public void handleSave() {
-        FileChooser chooser = new FileChooser();
-        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text files", "*.txt"));
-        File file = chooser.showSaveDialog(editorService.getEditorArea().getScene().getWindow());
-        if (file != null) {
-            String content = editorService.getEditorArea().getText();
-            editorService.saveText(file.getAbsolutePath(), content);
-        }
-    }
-
-    @FXML
-    private void handleRead() {
-        FileChooser chooser = new FileChooser();
-        File file = chooser.showOpenDialog(editorService.getEditorArea().getScene().getWindow());
-
-        if (file != null) {
-            String content = editorService.readText(file.getAbsolutePath());
-            editorService.getEditorArea().replaceText(content);
-        }
     }
 }
---
+++ src/main/java/com/quilca/controller/TopBarController.java
@@ -0,0 +1,48 @@
+package com.quilca.controller;
+
+import com.quilca.service.EditorService;
+import javafx.application.HostServices;
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Component
+public class TopBarController {
+
+    private final HostServices hostServices;
+    private final EditorService editorService;
+
+    public TopBarController(HostServices hostServices, EditorService editorService) {
+        this.hostServices = hostServices;
+        this.editorService = editorService;
+    }
+
+    @FXML
+    private void about() {
+        hostServices.showDocument("https://github.com/gabrielsh06/Quilca");
+    }
+
+    @FXML
+    public void handleSave() {
+        FileChooser chooser = new FileChooser();
+        chooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Text files", "*.txt"));
+        File file = chooser.showSaveDialog(editorService.getEditorArea().getScene().getWindow());
+        if (file != null) {
+            String content = editorService.getEditorArea().getText();
+            editorService.saveText(file.getAbsolutePath(), content);
+        }
+    }
+
+    @FXML
+    private void handleRead() {
+        FileChooser chooser = new FileChooser();
+        File file = chooser.showOpenDialog(editorService.getEditorArea().getScene().getWindow());
+
+        if (file != null) {
+            String content = editorService.readText(file.getAbsolutePath());
+            editorService.getEditorArea().replaceText(content);
+        }
+    }
+}
