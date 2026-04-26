e486ef25cad172ce72ac2b7175123b1ce4794dd4
gabrielsh06 <shgabriel06@gmail.com>
2026-04-26 00:15:09 -0500

    feat: add new TextStyle

--- src/main/java/com/quilca/model/TextStyle.java
+++ src/main/java/com/quilca/model/TextStyle.java
@@ -1,20 +1,47 @@
 package com.quilca.model;
 
-public record TextStyle(
-        boolean bold,
-        boolean italic,
-        int size,
-        String color)
-{
+import java.util.Set;
+import java.util.HashSet;
+
+public record TextStyle(Set<String> classes) {
+
     public static TextStyle forDefault() {
-        return new TextStyle(false, false, 14, "black");
+        return new TextStyle(new HashSet<>());
     }
 
-    public TextStyle withBold(boolean bold) {return new TextStyle(bold, this.italic, this.size, this.color);}
+    public TextStyle withBold(boolean bold) {
+        Set<String> newClasses = new HashSet<>(classes);
+        if (bold) newClasses.add("bold");
+        else newClasses.remove("bold");
+        return new TextStyle(newClasses);
+    }
 
-    public TextStyle withItalic(boolean italic) {return new TextStyle(this.bold, italic, this.size, this.color);}
+    public TextStyle withItalic(boolean italic) {
+        Set<String> newClasses = new HashSet<>(classes);
+        if (italic) newClasses.add("italic");
+        else newClasses.remove("italic");
+        return new TextStyle(newClasses);
+    }
 
-    public TextStyle withSize(int size) {return new TextStyle(this.bold, this.italic, size, this.color);}
+    public TextStyle withSize(int size) {
+        Set<String> newClasses = new HashSet<>(classes);
+        newClasses.removeIf(c -> c.startsWith("size-"));
+        newClasses.add("size-" + size);
+        return new TextStyle(newClasses);
+    }
 
-    public TextStyle withColor(String color) {return new TextStyle(this.bold, this.italic, this.size, color);}
+    public TextStyle withColor(String color) {
+        Set<String> newClasses = new HashSet<>(classes);
+        newClasses.removeIf(c -> c.startsWith("color-"));
+        newClasses.add(color);
+        return new TextStyle(newClasses);
+    }
+
+    public boolean isBold() {
+        return classes.contains("bold");
+    }
+
+    public boolean isItalic() {
+        return classes.contains("italic");
+    }
 }
